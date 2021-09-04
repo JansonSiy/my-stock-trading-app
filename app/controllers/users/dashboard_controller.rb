08c531da1c488d36932cff0cc64e4f3ece4a1756
Janson <jansonharold.siy@gmail.com>
2021-09-04 10:52:44 +0800

    fixed get api values and env

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -1,22 +1,12 @@
 class Users::DashboardController < ApplicationController
+  # use @ to use it within the class
+
   def index
-    client = IEX::Api::Client.new(
-      publishable_token: 'pk_1e6046c067f94660afa2f03aa72d47f1',
-      endpoint: 'https://cloud.iexapis.com/v1'
-    )
-    @msft_stock = client.quote('MSFT').latest_price
-    msft = client.company('MSFT')
-    @msft_ceo = msft.ceo
-    @msft_name = msft.company_name
-
-    @fb_stock = client.quote('FB').latest_price
-    fb = client.company('FB')
-    @fb_ceo = fb.ceo
-    @fb_name = fb.company_name
-
-    @twtr_stock = client.quote('TWTR').latest_price
-    twtr = client.company('TWTR')
-    @twtr_ceo = twtr.ceo
-    @twtr_name = twtr.company_name
+    client = Stock.iex
+    # Stock nag ququery sa database
+    Stock.all.each do |stock|
+      stock.update!(price: client.quote(stock.name).latest_price)
+    end
+    @stocks = Stock.all
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,21 @@
 class Stock < ApplicationRecord
+  # Pag walang self, maaaccess si iex gamit ng: 
+  # stock = Stock.new
+  # stock = stock.iex
+
+  # Pag may self, maaaccess si iex gamit ng: 
+  # Stock.iex
+  def self.iex
+    client = IEX::Api::Client.new(
+      publishable_token: ENV['iex_publishable_token'],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+  end
+
+  def self.sandbox
+    client = IEX::Api::Client.new(
+      publishable_token: ENV['sandbox_publishable_token'],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+  end
 end
