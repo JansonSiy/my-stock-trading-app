a257b64012661d851965a8be1203014c133ee9bf
Janson Harold Siy <jansonharoldsiy@Jansons-MacBook-Air.local>
2021-09-05 21:20:36 +0800

    edited env back

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,14 +2,14 @@ class Stock < ApplicationRecord
 # you can access Stock.iex in the controller
   def self.iex
     client = IEX::Api::Client.new(
-      publishable_token: 'pk_1e6046c067f94660afa2f03aa72d47f1',
+      publishable_token: ENV['iex_publishable_token'],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
   end
 
   def self.sandbox
     client = IEX::Api::Client.new(
-      publishable_token: 'Tpk_00513307abff4551a5e7240c1f0a8492',
+      publishable_token: ENV['sandbox_publishable_token'],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
   end
